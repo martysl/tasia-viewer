e9e13b822384a1cec96fb37dbcc6389ebc308527
LOstorm <lostorm@cock.li>
2023-09-29 14:35:00 +0000

    Fix texture upload quality

--- indra/llimagej2coj/llimagej2coj.cpp
+++ indra/llimagej2coj/llimagej2coj.cpp
@@ -427,7 +427,6 @@ public:
         opj_set_default_encoder_parameters(&parameters);
         parameters.cod_format = OPJ_CODEC_J2K;
         parameters.cp_disto_alloc = 1;
-        parameters.max_cs_size = (1 << 15);
 
         if (reversible)
         {
@@ -438,10 +437,10 @@ public:
         {
             parameters.tcp_numlayers = 5;
             parameters.tcp_rates[0] = 1920.0f;
-            parameters.tcp_rates[1] = 960.0f;
-            parameters.tcp_rates[2] = 480.0f;
-            parameters.tcp_rates[3] = 120.0f;
-            parameters.tcp_rates[4] = 30.0f;
+            parameters.tcp_rates[1] = 480.0f;
+            parameters.tcp_rates[2] = 120.0f;
+            parameters.tcp_rates[3] = 30.0f;
+            parameters.tcp_rates[4] = 10.0f;
             parameters.irreversible = 1;
             parameters.tcp_mct = 1;
         }
