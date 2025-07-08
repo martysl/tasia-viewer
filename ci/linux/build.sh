7d86f1045d373a2ec5a9f4d919fd4deda3bcb229
Freestorm <freestorm@/dev/null>
2025-07-08 21:09:31 +0100

    Looks to be all done - restore default order

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -2,6 +2,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git clone https://github.com/FirestormViewer/fs-build-variables.git
 export AUTOBUILD_VARIABLES_FILE="$(pwd)/fs-build-variables/variables"
 cd viewer
+echo "Setting fmod to file://$SCRIPT_DIR/artifacts/fmodstudio-2.02.24-linux64-242871432.tar.bz2"
 autobuild installables edit fmodstudio platform=linux64 hash=c2cb6978b5060fd178be389ca751a7a4 url=file://$SCRIPT_DIR/artifacts/fmodstudio-2.02.24-linux64-242871432.tar.bz2
 autobuild installables edit webrtc platform=linux64 hash=f94e4dbf13ad3e86e8036e4d24645ab4 url=file://$SCRIPT_DIR/artifacts/webrtc-m114_release.251141020-linux64-251141020.tar.bz2
 XZ_DEFAULTS=-T0 autobuild build -A 64 -c ReleaseFS_open -- --fmodstudio --avx2 --crashreporting --package --ninja -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=$BUGSPLAT_DATABASE
--- ci/windows/run.sh
+++ ci/windows/run.sh
@@ -8,6 +8,7 @@ cd viewer
 pip install -r requirements.txt
 git reset --hard $1
 git submodule update --init --recursive
+echo "Setting fmod to file://$SCRIPT_DIR/artifacts/fmodstudio-2.02.23-windows64-242661809.tar.bz2"
 autobuild installables edit fmodstudio platform=windows64 hash=f5844bc284eb47cd3e0642175eba80f1 url=file://$SCRIPT_DIR/artifacts/fmodstudio-2.02.23-windows64-242661809.tar.bz2
 autobuild configure -A 64 -c ReleaseFS_open -- --fmodstudio --avx2 --crashreporting --package -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=$BUGSPLAT_DATABASE
 XZ_DEFAULTS=-T0 autobuild build -A 64 -c ReleaseFS_open -- --fmodstudio --avx2 --crashreporting --package -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=$BUGSPLAT_DATABASE
