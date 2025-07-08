2223d240c2d82bf926ba3edcfe683a2a1f22ad6a
Freestorm <freestorm@/dev/null>
2025-07-08 21:33:31 +0100

    Check mac now

--- ci/mac/build.sh
+++ ci/mac/build.sh
@@ -10,6 +10,8 @@ export AUTOBUILD_VARIABLES_FILE="$(pwd)/fs-build-variables/variables"
 
 cd "$CI_PROJECT_DIR"
 cd viewer
+pip install -r requirements.txt
+
 echo "Installing fmodstudio: file://$CI_PROJECT_DIR/viewer/ci/mac/artifacts/fmodstudio-2.02.24-darwin64-242921144.tar.bz2"
 autobuild installables edit fmodstudio platform=darwin64 hash=2fa4a9dbb2365ef8c4b6acd735ef08e7 url=file://$CI_PROJECT_DIR/viewer/ci/mac/artifacts/fmodstudio-2.02.24-darwin64-242921144.tar.bz2
 XZ_DEFAULTS=-T0 autobuild build -A 64 -c ReleaseFS_open -- --fmodstudio --crashreporting --package -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=$BUGSPLAT_DATABASE
