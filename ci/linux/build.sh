97c1482c78cf04b4881145e8cf82a2a207357aac
Freestorm <freestorm@/dev/null>
2025-07-08 18:29:46 +0100

    Move build scripts into the repo (linux)

---
+++ ci/linux/build.sh
@@ -0,0 +1,5 @@
+export AUTOBUILD_VARIABLES_FILE=/home/build/viewer/fs-build-variables/variables
+cd viewer
+autobuild installables edit fmodstudio platform=linux64 hash=c2cb6978b5060fd178be389ca751a7a4 url=file:///home/build/viewer/3p-fmodstudio/fmodstudio-2.02.24-linux64-242871432.tar.bz2
+autobuild installables edit webrtc platform=linux64 hash=f94e4dbf13ad3e86e8036e4d24645ab4 url=file:///home/build/viewer/webrtc-m114_release.251141020-linux64-251141020.tar.bz2
+XZ_DEFAULTS=-T0 autobuild build -A 64 -c ReleaseFS_open -- --fmodstudio --avx2 --crashreporting --package --ninja -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=Freestorm
