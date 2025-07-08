15ab27dd02d2a54852c396ed050608c2e17c8824
Freestorm <freestorm@/dev/null>
2025-07-08 21:43:21 +0100

    Check windows fmod

--- ci/windows/run.sh
+++ ci/windows/run.sh
@@ -1,15 +1,20 @@
 set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WIN_SCRIPT_DIR=$(cygpath -w "$SCRIPT_DIR")
 
 git clone https://github.com/FirestormViewer/fs-build-variables.git
 export AUTOBUILD_VARIABLES_FILE="$(pwd)/fs-build-variables/variables"
 
 cd viewer
 git reset --hard $1
+
+python3 -m venv "$(pwd)/venv"
+source "$(pwd)/venv/bin/activate"
 pip install -r requirements.txt
+
 git submodule update --init --recursive
-echo "Setting fmod to file://$SCRIPT_DIR/artifacts/fmodstudio-2.02.23-windows64-242661809.tar.bz2"
-autobuild installables edit fmodstudio platform=windows64 hash=f5844bc284eb47cd3e0642175eba80f1 url=file://$SCRIPT_DIR/artifacts/fmodstudio-2.02.23-windows64-242661809.tar.bz2
+echo "Setting fmod to file://${WIN_SCRIPT_DIR//\\//}/artifacts/fmodstudio-2.02.23-windows64-242661809.tar.bz2"
+autobuild installables edit fmodstudio platform=windows64 hash=f5844bc284eb47cd3e0642175eba80f1 url=file://${WIN_SCRIPT_DIR//\\//}/artifacts/fmodstudio-2.02.23-windows64-242661809.tar.bz2
 autobuild configure -A 64 -c ReleaseFS_open -- --fmodstudio --avx2 --crashreporting --package -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=$BUGSPLAT_DATABASE
 XZ_DEFAULTS=-T0 autobuild build -A 64 -c ReleaseFS_open -- --fmodstudio --avx2 --crashreporting --package -DUSE_BUGSPLAT=ON -DBUGSPLAT_DB=$BUGSPLAT_DATABASE
 
