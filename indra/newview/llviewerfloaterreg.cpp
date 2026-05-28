35463bcf1e30f3455599a73fc23e559bf0573c03
tasia <tasia@easierit.org>
2026-05-28 22:33:08 +0200

    Comment out more PBR-only includes/registrations: materialeditor

--- indra/newview/llviewerfloaterreg.cpp
+++ indra/newview/llviewerfloaterreg.cpp
@@ -164,7 +164,7 @@
 #include "llinspectobject.h"
 #include "llinspectremoteobject.h"
 #include "llinspecttoast.h"
-#include "llmaterialeditor.h"
+//#include "llmaterialeditor.h" // PBR-only
 #include "llmoveview.h"
 // <FS:Ansariel> [FS communication UI]
 //#include "llfloaterimnearbychat.h
@@ -563,8 +563,8 @@ void LLViewerFloaterReg::registerFloaters()
     LLFloaterReg::add("save_camera_preset", "floater_save_camera_preset.xml", (LLFloaterBuildFunc)&LLFloaterReg::build<LLFloaterSaveCameraPreset>);
     LLFloaterReg::add("script_colors", "floater_script_ed_prefs.xml", (LLFloaterBuildFunc)&LLFloaterReg::build<LLFloaterScriptEdPrefs>);
 
-    LLFloaterReg::add("material_editor", "floater_material_editor.xml", (LLFloaterBuildFunc)&LLFloaterReg::build<LLMaterialEditor>);
-    LLFloaterReg::add("live_material_editor", "floater_live_material_editor.xml", (LLFloaterBuildFunc)&LLFloaterReg::build<LLMaterialEditor>);
+    //LLFloaterReg::add("material_editor", "floater_material_editor.xml", (LLFloaterBuildFunc)&LLFloaterReg::build<LLMaterialEditor>); // PBR-only
+    //LLFloaterReg::add("live_material_editor", "floater_live_material_editor.xml", (LLFloaterBuildFunc)&LLFloaterReg::build<LLMaterialEditor>); // PBR-only
 
     LLFloaterReg::add("telehubs", "floater_telehub.xml",&LLFloaterReg::build<LLFloaterTelehub>);
     LLFloaterReg::add("test_inspectors", "floater_test_inspectors.xml", &LLFloaterReg::build<LLFloaterTestInspectors>);
