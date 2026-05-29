4271ddbcfe8e648a7c4c06065392535cb5ce3882
tasia <tasia@easierit.org>
2026-05-30 01:35:15 +0200

    Fix remaining postToMainCoro in llvoicewebrtc.cpp, replace with direct call

--- indra/newview/llvoicewebrtc.cpp
+++ indra/newview/llvoicewebrtc.cpp
@@ -435,7 +435,7 @@ void LLWebRTCVoiceClient::notifyStatusObservers(LLVoiceClientStatusObserver::ESt
 
         if (voice_status)
         {
-            LLAppViewer::instance()->postToMainCoro([=]() { LLFirstUse::speak(true); });
+            LLFirstUse::speak(true);
         }
     }
 }
