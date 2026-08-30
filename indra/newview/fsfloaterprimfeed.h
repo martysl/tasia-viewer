491a1f933ba19ac6a60f936ef160c0d426d59063
tasia <tasia@easierit.org>
2026-08-30 15:34:39 +0200

    macOS: disable fatal warnings for mac build (GCC_DISABLE_FATAL_WARNINGS) + mark last onVisibilityChange override

--- indra/newview/fsfloaterprimfeed.h
+++ indra/newview/fsfloaterprimfeed.h
@@ -110,7 +110,7 @@ public:
     void draw() override;
 
 private:
-    void onVisibilityChange(bool new_visibility);
+    void onVisibilityChange(bool new_visibility) override;
     void primfeedAuthResponse(bool success, const LLSD& response);
     bool onPrimfeedConnectStateChange(const LLSD& data);
     bool onPrimfeedConnectInfoChange();
